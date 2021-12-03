39a06498f9a64b740bb17339c4cda9697eede441
NBasheer <nbasheer@korewireless.com>
2021-12-03 15:41:03 +0530

    Injected dependency. Injected remote data source with Food Recipes API.

--- app/src/main/java/com/example/receipeapplication/RemoteDataSource.kt
+++ app/src/main/java/com/example/receipeapplication/RemoteDataSource.kt
@@ -1,4 +1,18 @@
 package com.example.receipeapplication
 
-class RemoteDataSource() {
+import FoodRecipe
+import retrofit2.Response
+import javax.inject.Inject
+
+
+//RemoteDataSource fetch data from remote food recipe API(spoonacular API)
+
+class RemoteDataSource @Inject constructor(
+    private val foodRecipesApi: FoodRecipesApi //injecting API
+) {
+    suspend fun getRecipes(queries: Map<String, String>): Response<FoodRecipe>{
+        return foodRecipesApi.getRecipes(queries)
+
+    }
+
 }
