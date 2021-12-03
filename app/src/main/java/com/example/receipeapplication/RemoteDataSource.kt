74666e48094b1407fc45b08f8f5f7d40f7ca7263
NBasheer <nbasheer@korewireless.com>
2021-12-03 15:27:12 +0530

    Created NetworkModule.

---
+++ app/src/main/java/com/example/receipeapplication/RemoteDataSource.kt
@@ -0,0 +1,4 @@
+package com.example.receipeapplication
+
+class RemoteDataSource() {
+}
---
+++ app/src/main/java/com/example/receipeapplication/di/NetworkModule.kt
@@ -0,0 +1,57 @@
+package com.example.receipeapplication.di
+
+import com.example.receipeapplication.Constants.Companion.BASE_URL
+import com.example.receipeapplication.FoodRecipesApi
+import dagger.Module
+import dagger.Provides
+
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponent::class) //all those bindings inside this network module will be available in an application component.
+//this network module where we're going to tell Hilt how to provide retrofit builder to our remote data source.
+object NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideHttpClient(): OkHttpClient{
+        return OkHttpClient.Builder()
+            .readTimeout(15, TimeUnit.SECONDS)
+            .connectTimeout(15, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideConverterFactory():GsonConverterFactory{
+        return GsonConverterFactory.create()
+
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofitInstance(
+        okHttpClient: OkHttpClient,
+        gsonConverterFactory: GsonConverterFactory
+    ): Retrofit{
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(gsonConverterFactory)
+            .build()
+
+    }
+
+
+    @Singleton
+    @Provides
+    fun provideApiService(retrofit: Retrofit): FoodRecipesApi{
+        return retrofit.create(FoodRecipesApi::class.java)
+    }
+}
