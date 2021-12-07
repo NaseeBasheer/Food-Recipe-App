ab240dc2f3c49ed7779a75005c5820edfa254934
NBasheer <nbasheer@korewireless.com>
2021-12-07 12:23:07 +0530

    Loaded data to the recycler view in recipes fragment.

--- app/src/main/java/com/example/receipeapplication/bindingadapters/RecipesRowBinding.kt
+++ app/src/main/java/com/example/receipeapplication/bindingadapters/RecipesRowBinding.kt
@@ -5,12 +5,22 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
+import coil.load
 import com.example.receipeapplication.R
 
 class RecipesRowBinding {
 
 
     companion object{
+
+        @BindingAdapter("loadImageFromUrl")
+        @JvmStatic
+        fun loadImageFromUrl(imageView: ImageView, imageUrl: String){
+            imageView.load(imageUrl){
+                crossfade(600)
+            }
+        }
+
 
         @BindingAdapter("setNumberOfLikes")
         @JvmStatic
@@ -52,5 +62,7 @@ class RecipesRowBinding {
             }
 
         }
+
+
     }
 }
