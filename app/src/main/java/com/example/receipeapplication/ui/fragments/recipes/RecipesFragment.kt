61c0f134f9fc2bf27330c65cedbeb63524d4feda
NBasheer <nbasheer@korewireless.com>
2021-12-09 16:20:16 +0530

    Designed RecipesFragment Menu.

--- app/src/main/java/com/example/receipeapplication/ui/fragments/recipes/RecipesFragment.kt
+++ app/src/main/java/com/example/receipeapplication/ui/fragments/recipes/RecipesFragment.kt
@@ -2,10 +2,9 @@ package com.example.receipeapplication.ui.fragments.recipes
 
 import android.os.Bundle
 import android.util.Log
+import android.view.*
+import android.widget.SearchView
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
@@ -29,7 +28,7 @@ import kotlinx.coroutines.launch
 
 @ExperimentalCoroutinesApi
 @AndroidEntryPoint
-class RecipesFragment : Fragment() {
+class RecipesFragment : Fragment(), SearchView.OnQueryTextListener {
     private val args by navArgs<RecipesFragmentArgs>()
 
     private lateinit var networkListener: NetworkListener
@@ -59,6 +58,8 @@ class RecipesFragment : Fragment() {
        _binding =  FragmentRecipesBinding.inflate(inflater, container, false)
         binding.lifecycleOwner = this
         binding.mainViewModel = mainViewModel
+
+        setHasOptionsMenu(true)
 
         setupRecyclerView()
         recipesViewModel.readBackOnline.observe(viewLifecycleOwner,{
@@ -93,6 +94,23 @@ class RecipesFragment : Fragment() {
         binding.recyclerview.adapter =mAdapter
         binding.recyclerview.layoutManager = LinearLayoutManager(requireContext())
         showShimmerEffect()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.recipes_menu, menu)
+
+        val search  = menu.findItem(R.id.menu_search)
+        val searchView = search.actionView as? SearchView
+        searchView?.isSubmitButtonEnabled = true
+        searchView?.setOnQueryTextListener(this)
+    }
+    override fun onQueryTextSubmit(query: String?): Boolean {
+
+        return true
+    }
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+        return true
     }
 
     private fun readDatabase() {
@@ -157,5 +175,7 @@ class RecipesFragment : Fragment() {
         super.onDestroy()
         _binding = null
     }
+
+
 
 }
