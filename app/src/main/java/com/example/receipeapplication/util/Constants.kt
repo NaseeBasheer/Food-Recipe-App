44c1840f3f187721c96326e75a678fa5dc279177
NBasheer <nbasheer@korewireless.com>
2021-12-09 11:52:47 +0530

    Handled selection of chip and return from bottom sheet.

--- app/src/main/java/com/example/receipeapplication/util/Constants.kt
+++ app/src/main/java/com/example/receipeapplication/util/Constants.kt
@@ -12,7 +12,7 @@ class Constants {
         const val QUERY_API_KEY = "apiKey"
         const val QUERY_TYPE = "type"
         const val QUERY_DIET = "diet"
-        const val QUERY_ADD_RECIPE_INFO = "addRecipeInformation"
+        const val QUERY_ADD_RECIPE_INFORMATION = "addRecipeInformation"
         const val QUERY_FILL_INGREDIENTS = "fillIngredients"
 
         //Room DB
--- app/src/main/java/com/example/receipeapplication/viewmodels/RecipesViewModel.kt
+++ app/src/main/java/com/example/receipeapplication/viewmodels/RecipesViewModel.kt
@@ -8,17 +8,19 @@ import com.example.receipeapplication.util.Constants.Companion.API_KEY
 import com.example.receipeapplication.util.Constants.Companion.DEFAULT_DIET_TYPE
 import com.example.receipeapplication.util.Constants.Companion.DEFAULT_MEAL_TYPE
 import com.example.receipeapplication.util.Constants.Companion.DEFAULT_RECIPES_NUMBER
-import com.example.receipeapplication.util.Constants.Companion.QUERY_ADD_RECIPE_INFO
+import com.example.receipeapplication.util.Constants.Companion.QUERY_ADD_RECIPE_INFORMATION
 import com.example.receipeapplication.util.Constants.Companion.QUERY_API_KEY
 import com.example.receipeapplication.util.Constants.Companion.QUERY_DIET
 import com.example.receipeapplication.util.Constants.Companion.QUERY_FILL_INGREDIENTS
 import com.example.receipeapplication.util.Constants.Companion.QUERY_NUMBER
 import com.example.receipeapplication.util.Constants.Companion.QUERY_TYPE
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+@HiltViewModel
 class RecipesViewModel @Inject constructor(application: Application, private val dataStoreRepository: DataStoreRepository): AndroidViewModel(application) {
 
     private var mealType = DEFAULT_MEAL_TYPE
@@ -26,29 +28,26 @@ class RecipesViewModel @Inject constructor(application: Application, private val
 
     val readMealAndDietType = dataStoreRepository.readMealAndDietType
 
+    fun saveMealAndDietType(mealType: String, mealTypeId: Int, dietType: String, dietTypeId: Int) =
+        viewModelScope.launch(Dispatchers.IO) {
+            dataStoreRepository.saveMealAndDietType(mealType, mealTypeId, dietType, dietTypeId)
+        }
 
-    fun saveMealAndDietType(
-        mealType:String, mealTypeId: Int, dietType: String, dietTypeId: Int
-    )= viewModelScope.launch(Dispatchers.IO) {
-        dataStoreRepository.saveMealAndDietType(mealType, mealTypeId, dietType, dietTypeId)
-    }
+    fun applyQueries(): HashMap<String, String> {
+        val queries: HashMap<String, String> = HashMap()
 
-     fun applyQueries(): HashMap<String, String>{
+        viewModelScope.launch {
+            readMealAndDietType.collect { value ->
+                mealType = value.selectedMealType
+                dietType = value.selectedDietType
+            }
+        }
 
-         viewModelScope.launch {
-             readMealAndDietType.collect { value ->
-                 mealType = value.selectedMealType
-                 dietType = value.selectedDietType
-             }
-         }
-
-        val queries:HashMap<String, String> =HashMap()
-
-        queries[QUERY_NUMBER]= DEFAULT_RECIPES_NUMBER
-        queries[QUERY_API_KEY]= API_KEY
-        queries[QUERY_TYPE] = mealType
-        queries[QUERY_DIET]= dietType
-        queries[QUERY_ADD_RECIPE_INFO]= "true"
+        queries[QUERY_NUMBER] = DEFAULT_RECIPES_NUMBER
+        queries[QUERY_API_KEY] = API_KEY
+        queries[QUERY_TYPE] = mealType //Default
+        queries[QUERY_DIET] = dietType
+        queries[QUERY_ADD_RECIPE_INFORMATION] = "true"
         queries[QUERY_FILL_INGREDIENTS] = "true"
 
         return queries
