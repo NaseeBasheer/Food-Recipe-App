b75d18f0787d72484bdbb89cdf06b42f82614283
NBasheer <nbasheer@korewireless.com>
2021-12-14 18:26:58 +0530

    CReated Ingredients Adapter class.

--- app/src/main/java/com/example/receipeapplication/util/Constants.kt
+++ app/src/main/java/com/example/receipeapplication/util/Constants.kt
@@ -5,6 +5,7 @@ class Constants {
 
     companion object{
         const val BASE_URL = "https://api.spoonacular.com"
+        const val BASE_IMAGE_URL = "https://spoonacular.com/cdn/ingredients_100x100/"
         const val API_KEY = "28b3ea3376464fbdaf2de0717b04bc97"
 
         //API query keys
