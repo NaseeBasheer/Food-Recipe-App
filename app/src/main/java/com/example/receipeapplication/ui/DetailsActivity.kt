eeb8a870c6ae0c176db94b13e249249b407bf0e2
NBasheer <nbasheer@korewireless.com>
2021-12-15 14:41:49 +0530

    Created details menu.

--- app/src/main/java/com/example/receipeapplication/ui/DetailsActivity.kt
+++ app/src/main/java/com/example/receipeapplication/ui/DetailsActivity.kt
@@ -2,6 +2,7 @@ package com.example.receipeapplication.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import android.view.MenuItem
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
@@ -54,6 +55,10 @@ class DetailsActivity : AppCompatActivity() {
         }.attach()
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.details_menu, menu)
+        return true
+    }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (item.itemId == android.R.id.home) {
