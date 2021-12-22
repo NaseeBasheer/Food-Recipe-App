629e845c42ea733ac584d71079386f7a0f539ff3
NBasheer <nbasheer@korewireless.com>
2021-12-22 11:30:07 +0530

    Applied contextual action mode style.

--- app/src/main/java/com/example/receipeapplication/adapters/FavoriteRecipesAdapter.kt
+++ app/src/main/java/com/example/receipeapplication/adapters/FavoriteRecipesAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.receipeapplication.adapters
 
 import android.view.*
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.FragmentActivity
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.DiffUtil
@@ -34,6 +35,7 @@ class FavoriteRecipesAdapter(private val requireActivity: FragmentActivity): Rec
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         return MyViewHolder.from(parent)
+
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
@@ -58,15 +60,11 @@ class FavoriteRecipesAdapter(private val requireActivity: FragmentActivity): Rec
         return favoriteRecipes.size
     }
 
-    fun setData(newFavoriteRecipes: List<FavoritesEntity>){
-        val favoriteRecipesDiffUtil = RecipesDiffUtil(favoriteRecipes,  newFavoriteRecipes)
-        val diffUtilResult = DiffUtil.calculateDiff(favoriteRecipesDiffUtil)
-        favoriteRecipes = newFavoriteRecipes
-        diffUtilResult.dispatchUpdatesTo(this)
-    }
+
 
     override fun onCreateActionMode(actionMode: ActionMode?, menu: Menu?): Boolean {
         actionMode?.menuInflater?.inflate(R.menu.favorites_contextual_menu, menu)
+        applyStatusBarColor(R.color.contextualStatusBarColor)
         return true
     }
 
@@ -79,6 +77,17 @@ class FavoriteRecipesAdapter(private val requireActivity: FragmentActivity): Rec
     }
 
     override fun onDestroyActionMode(mode: ActionMode?) {
+        applyStatusBarColor(R.color.statusBarColor)
 
+    }
+    private fun applyStatusBarColor(color: Int){
+        requireActivity.window.statusBarColor =
+            ContextCompat.getColor(requireActivity, color)
+    }
+    fun setData(newFavoriteRecipes: List<FavoritesEntity>){
+        val favoriteRecipesDiffUtil = RecipesDiffUtil(favoriteRecipes,  newFavoriteRecipes)
+        val diffUtilResult = DiffUtil.calculateDiff(favoriteRecipesDiffUtil)
+        favoriteRecipes = newFavoriteRecipes
+        diffUtilResult.dispatchUpdatesTo(this)
     }
 }
