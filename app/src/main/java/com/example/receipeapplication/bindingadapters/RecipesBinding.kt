9ab6e353263ba2ee10cc2eaa0bf6726c662aa24e
NBasheer <nbasheer@korewireless.com>
2022-01-10 15:02:42 +0530

    Code Cleanup - RecipesBinding & RecipesRowBinding.

--- app/src/main/java/com/example/receipeapplication/bindingadapters/RecipesBinding.kt
+++ app/src/main/java/com/example/receipeapplication/bindingadapters/RecipesBinding.kt
@@ -3,6 +3,7 @@ package com.example.receipeapplication.bindingadapters
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.view.isVisible
 import androidx.databinding.BindingAdapter
 import com.example.receipeapplication.data.database.entities.RecipesEntity
 import com.example.receipeapplication.models.FoodRecipe
@@ -10,47 +11,24 @@ import com.example.receipeapplication.util.NetworkResult
 
 class RecipesBinding {
 
-    companion object{
+    companion object {
 
-        @BindingAdapter("readApiResponse", "readDatabase", requireAll = true)
+        @BindingAdapter("readApiResponse","readDatabase",requireAll = true)
         @JvmStatic
-        fun errorImageViewVisibility(
-            imageView: ImageView,
+        fun handleReadDataErrors(
+            view: View,
             apiResponse: NetworkResult<FoodRecipe>?,
             database: List<RecipesEntity>?
         ){
-            if(apiResponse is NetworkResult.Error && database.isNullOrEmpty()){
-                imageView.visibility = View.VISIBLE
-
+            when (view){
+                is ImageView ->{
+                    view.isVisible = apiResponse is NetworkResult.Error && database.isNullOrEmpty()
+                }
+                is TextView ->{
+                    view.isVisible = apiResponse is NetworkResult.Error && database.isNullOrEmpty()
+                    view.text = apiResponse?.message.toString()
+                }
             }
-            else if(apiResponse is NetworkResult.Loading){
-                imageView.visibility = View.INVISIBLE
-            }
-            else if(apiResponse is NetworkResult.Success){
-                imageView.visibility = View.INVISIBLE
-            }
-
-        }
-
-        @BindingAdapter("readApiResponse2", "readDatabase2", requireAll = true)
-        @JvmStatic
-        fun errorTextViewVisibility(
-            textView: TextView,
-            apiResponse: NetworkResult<FoodRecipe>?,
-            database: List<RecipesEntity>?
-        ){
-            if(apiResponse is NetworkResult.Error && database.isNullOrEmpty()){
-                textView.visibility = View.VISIBLE
-                textView.text = apiResponse.message.toString()
-
-            }
-            else if(apiResponse is NetworkResult.Loading){
-                textView.visibility = View.INVISIBLE
-            }
-            else if(apiResponse is NetworkResult.Success){
-                textView.visibility = View.INVISIBLE
-            }
-
         }
     }
 }
