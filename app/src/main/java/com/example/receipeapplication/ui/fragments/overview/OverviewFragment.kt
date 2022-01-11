dd66fe897c5e7b81d6b56b82e83ef85b4da42021
NBasheer <nbasheer@korewireless.com>
2022-01-11 15:05:53 +0530

    Code cleanup in OverviewFragment.

--- app/src/main/java/com/example/receipeapplication/ui/fragments/overview/OverviewFragment.kt
+++ app/src/main/java/com/example/receipeapplication/ui/fragments/overview/OverviewFragment.kt
@@ -5,9 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.core.content.ContextCompat
 import coil.load
 import com.example.receipeapplication.R
+import com.example.receipeapplication.bindingadapters.RecipesRowBinding
 import com.example.receipeapplication.databinding.FragmentInstructionsBinding
 import com.example.receipeapplication.databinding.FragmentOverviewBinding
 import com.example.receipeapplication.models.Result
@@ -23,53 +26,38 @@ class OverviewFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         _binding =  FragmentOverviewBinding.inflate(inflater, container, false)
         val args = arguments
-        val myBundle: Result? = args?.getParcelable(RECIPE_RESULT_KEY)
-        binding.mainImageView.load(myBundle?.image)
-        binding.titleTextView.text = myBundle?.title
-        binding.likesTextView.text = myBundle?.aggregateLikes.toString()
-        binding.timeTextView.text = myBundle?.readyInMinutes.toString()
-        myBundle?.summary.let {
+        val myBundle: Result = args!!.getParcelable<Result>(RECIPE_RESULT_KEY) as Result
+        binding.mainImageView.load(myBundle.image)
+        binding.titleTextView.text = myBundle.title
+        binding.likesTextView.text = myBundle.aggregateLikes.toString()
+        binding.timeTextView.text = myBundle.readyInMinutes.toString()
+        /*myBundle?.summary.let {
             val summary = Jsoup.parse(it).text()
             binding.summaryTextView.text = summary
-        }
-
-
-        if(myBundle?.vegetarian == true){
-            binding.vegetarianImageView.setColorFilter(ContextCompat.getColor(requireContext(), R.color.green))
-            binding.vegetarianTextView.setTextColor(ContextCompat.getColor(requireContext(), R.color.green))
-        }
-
-        /*if(myBundle?.vegetarian == true){
-            binding.veganImageView.setColorFilter(ContextCompat.getColor(requireContext(), R.color.green))
-            binding.veganTextView.setTextColor(ContextCompat.getColor(requireContext(), R.color.green))
         }*/
-        if(myBundle?.vegan == true){
-            binding.veganImageView.setColorFilter(ContextCompat.getColor(requireContext(), R.color.green))
-            binding.veganTextView.setTextColor(ContextCompat.getColor(requireContext(), R.color.green))
-        }
-        if(myBundle?.cheap == true){
-            binding.cheapImageView.setColorFilter(ContextCompat.getColor(requireContext(), R.color.green))
-            binding.cheapTextView.setTextColor(ContextCompat.getColor(requireContext(), R.color.green))
-        }
-        if(myBundle?.glutenFree == true){
-            binding.glutenFreeImageView.setColorFilter(ContextCompat.getColor(requireContext(), R.color.green))
-            binding.glutenFreeTextView.setTextColor(ContextCompat.getColor(requireContext(), R.color.green))
-        }
-        if(myBundle?.dairyFree == true){
-            binding.dairyFreeImageView.setColorFilter(ContextCompat.getColor(requireContext(), R.color.green))
-            binding.dairyFreeTextView.setTextColor(ContextCompat.getColor(requireContext(), R.color.green))
-        }
-        if(myBundle?.veryHealthy == true){
-            binding.healthyImageView.setColorFilter(ContextCompat.getColor(requireContext(), R.color.green))
-            binding.healthyTextView.setTextColor(ContextCompat.getColor(requireContext(), R.color.green))
-        }
+        RecipesRowBinding.parseHtml(binding.summaryTextView, myBundle.summary)
+
+
+        updateColors(myBundle.vegetarian, binding.vegetarianTextView, binding.vegetarianImageView)
+        updateColors(myBundle.vegan, binding.veganTextView, binding.veganImageView)
+        updateColors(myBundle.cheap, binding.cheapTextView, binding.cheapImageView)
+        updateColors(myBundle.dairyFree, binding.dairyFreeTextView, binding.dairyFreeImageView)
+        updateColors(myBundle.glutenFree, binding.glutenFreeTextView, binding.glutenFreeImageView)
+        updateColors(myBundle.veryHealthy, binding.healthyTextView, binding.healthyImageView)
 
 
         return binding.root
+    }
+
+    private fun updateColors(stateIsOn: Boolean, textView: TextView, imageView: ImageView) {
+        if (stateIsOn) {
+            imageView.setColorFilter(ContextCompat.getColor(requireContext(),R.color.green))
+            textView.setTextColor(ContextCompat.getColor(requireContext(), R.color.green))
+        }
     }
 
     override fun onDestroyView() {
